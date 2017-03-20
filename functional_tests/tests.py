4b50e24bbbdd238eb648f79307b057e32d0a9623
Yifan Liao <yifanyangliao@gmail.com>
2017-03-20 11:32:51 -0700

    Hack the FT runner in a smarter way, and move db outside of main source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,6 @@
 import time
 import sys
+import os
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -11,17 +12,11 @@ MAX_WAIT = 10
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        for arg in sys.argv:
-            if 'liveserver' in arg:
-                cls.server_url = 'http://' + arg.split('=')[1]
-                return
-        super().setUpClass()
-        cls.server_url = cls.live_server_url
-
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            setattr(self, 'live_server_url', 'http://' + staging_server)
 
     def tearDown(self):
         self.browser.quit()
@@ -45,7 +40,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage.
-        self.browser.get(self.server_url)
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -86,7 +81,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new todo list
-        self.browser.get(self.server_url)
+        self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
@@ -105,7 +100,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Francis visits the home page. There is no sign of Edith's
         # list.
-        self.browser.get(self.server_url)
+        self.browser.get(self.live_server_url)
         page_next = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_next)
         self.assertNotIn('make a fly', page_next)
@@ -131,7 +126,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
-        self.browser.get(self.server_url)
+        self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
